1a5abafd4c5180089c5758d98731a12b72a7fa56
Gabriele Corbo <gabricorbo99@gmail.com>
2021-07-02 14:05:03 +0200

    inizio script

--- progetto.r
+++ progetto.r
@@ -1,1 +1,15 @@
-#sesso
+f <- file.choose()
+wine <- read.csv(f)
+
+View(wine)
+# Dimensioni
+dim(wine)
+# Overview delle prime righe
+head(wine)
+
+#Look at the main statistics for each covariate:
+summary(wine)
+
+g = lm( pH ~ ., data = wine )
+
+summary( g )   #sembra fico
