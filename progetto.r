a8f2711eff6177b9fd328d4fe4f3e87a8473d7cf
davidowicz <davidefabroni99@gmail.com>
2021-07-02 14:17:20 +0200

    Update progetto.r

--- progetto.r
+++ progetto.r
@@ -1,3 +1,16 @@
+library( car )
+library( faraway )
+library( leaps )
+library(MASS)
+library( GGally)
+library(rgl)
+library(dplyr)
+library(data.table)
+library(ggplot2)
+library(corrplot)
+library(RColorBrewer)
+
+
 f <- file.choose()
 wine <- read.csv(f)
 
@@ -10,6 +23,13 @@ head(wine)
 #Look at the main statistics for each covariate:
 summary(wine)
 
+ggpairs(wine)
+
 g = lm( pH ~ ., data = wine )
 
 summary( g )   #sembra fico
+
+plot(g,which=1)#NO OMOSCHEDASTICITÃ, noto nadamento parabolico 
+shapiro.test(g$residuals) #OK
+plot(dataADJ$GDP,dataADJ$Happi)
+abline(a=reg$coefficients[1],b=reg$coefficients[2])#Non Ã¨ una bella interpolazione
