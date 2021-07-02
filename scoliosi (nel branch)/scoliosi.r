7b428a89c7e9d636e5c65dd0757611f7fa8414dc
davidowicz <davidefabroni99@gmail.com>
2021-07-02 17:13:58 +0200

    organizzazione

---
+++ scoliosi (nel branch)/scoliosi.r
@@ -0,0 +1,40 @@
+library( car )
+library( faraway )
+library( leaps )
+library(MASS)
+library( GGally)
+library(rgl)
+library(dplyr)
+library(data.table)
+library(ggplot2)
+library(corrplot)
+library(RColorBrewer)
+
+
+f <- file.choose()
+scoliosi <- read.csv(f)
+
+View(scoliosi)
+# Dimensioni
+dim(scoliosi)
+# Overview delle prime righe
+head(scoliosi)
+
+print(sapply(scoliosi,function(x) any(is.na(x)))) 
+print(sapply(scoliosi, typeof)) 
+
+#Look at the main statistics for each covariate:
+summary(scoliosi)
+
+x11()
+ggpairs(scoliosi)
+
+g = lm( lumbar_lordosis_angle ~ .-class, data = scoliosi )
+
+summary( g )   #sembra fico
+
+plot(g,which=1)#NO OMOSCHEDASTICITÃ, noto nadamento parabolico 
+shapiro.test(g$residuals) 
+
+qqnorm( g$res, ylab = "Raw Residuals", pch = 16 )
+qqline( g$res )
