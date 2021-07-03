056a974d8651271f35302b7a54db293b0e95221d
Teo Bucci <teobucci8@gmail.com>
2021-07-03 13:19:34 +0200

    ulteriori commenti

--- scoliosi (nel branch)/scoliosi.r
+++ scoliosi (nel branch)/scoliosi.r
@@ -171,33 +171,55 @@ qqnorm( g_post_rs$res, ylab = "Raw Residuals", pch = 16 )
 qqline( g_post_rs$res )
 
 
-
-
-
-
-#uso boxcox
+# Per ottenere la normalita' facciamo BOX COX
 x11()
-b = boxcox(gl)
+b = boxcox(g_post_rs)
 best_lambdagl = b$x[ which.max( b$y ) ]
 best_lambdagl
+# lambda = 0.6262626
 
-gb = lm( (lumbar_lordosis_angle^best_lambdagl -1)/best_lambdagl ~ .-class-sacral_slope, data=scoliosi,subset = ( abs(stud) < 2 ) )
-summary( gb )
+# generiamo il nuovo LM dove modelliamo una funzione della risposta
+g_post_bc = lm( (lumbar_lordosis_angle^best_lambdagl -1)/best_lambdagl ~ .-class-sacral_slope, data=scoliosi,subset = ( abs(stud) < 2 ) )
+summary( g_post_bc )
+# R^2_adj diminuisce da 0.7261 a 0.725, nessun problema
+# ci sono covariate poco significative
 
-plot(gb,which=1)#noto omoschedasticita dei residui
-shapiro.test(gb$residuals) #ho normalita
 
-qqnorm( gb$res, ylab = "Raw Residuals", pch = 16 )
-qqline( gb$res )
+# LE IPOTESI SONO VERIFICATE
 
-gk = lm( (lumbar_lordosis_angle^best_lambdagl -1)/best_lambdagl ~ .-class-sacral_slope-pelvic_radius, data=scoliosi,subset = ( abs(stud) < 2 ) )
-summary( gk )
+plot(g_post_bc,which=1)
+# omoschedasticita dei residui
 
-plot(gk,which=1)#noto omoschedasticita dei residui
-shapiro.test(gk$residuals) #ho normalita
+shapiro.test(g_post_bc$residuals)
+# p-value = 0.196 => non rifiutiamo la normalita'
 
-qqnorm( gk$res, ylab = "Raw Residuals", pch = 16 )
-qqline( gk$res )
+qqnorm( g_post_bc$res, ylab = "Raw Residuals", pch = 16 )
+qqline( g_post_bc$res )
+
+
+
+# SELEZIONE COVARIATE
+
+# rimuoviamo "pelvic_radius" che ha un p-value "one-at-a-time" di 0.0755,
+# c'è evidenza per dire che non è significativo
+g_without_pr = lm( (lumbar_lordosis_angle^best_lambdagl -1)/best_lambdagl ~ .-class-sacral_slope-pelvic_radius, data=scoliosi,subset = ( abs(stud) < 2 ) )
+summary( g_without_pr )
+# R^2_adj scende da 0.725 a 0.723, ma semplifica di molto il modello, quindi ok
+
+# riverifichiamo le ipotesi
+plot(g_without_pr,which=1) #noto omoschedasticita dei residui
+shapiro.test(g_without_pr$residuals) #ho normalita, p-value da 0.196 a 0.22, migliora la normalita'
+
+qqnorm( g_without_pr$res, ylab = "Raw Residuals", pch = 16 )
+qqline( g_without_pr$res )
+
+
+
+
+
+
+
+
 
 #ANOVA
 
@@ -248,22 +270,6 @@ summary(gA)  #meglio, tutti significativi
 anova(gA)  #pvalue basso, rifiuto hp tutte le medie sono uguali
 
 
-
-#4)Modello reg : studio dei punti influenti
-x11()
-influencePlot( gk, id.method = "identify", main = "influential Plot",
-               sub = "Circle size is proportial to Cook's Distance" )
-
-
-influenti_nomi=row.names(influencePlot( reg, main = "influential Plot"))#, id=list(method="identify")))
-influenti=c()
-for (names in influenti_nomi)
-  influenti=c(influenti,which(scoliosi$class==names))
-influenti
-scoliosi$class[influenti]
-
-Levscol = scoliosi[-influenti,]
-Levscol = Levscol[seq(4,9,1)]
 
 #5) int conf e prev
 gp=lm(scoliosi$lumbar_lordosis_angle~scoliosi$pelvic_incidence,data=scoliosi)
